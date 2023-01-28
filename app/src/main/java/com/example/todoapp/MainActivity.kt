e2b2fb7c9ebb656be730db33ba3fb73875371bef
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-01-28 16:05:37 +0200

    + splash screen

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.todoapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        android.os.Handler(Looper.getMainLooper()).postDelayed(
+            {
+                val intent = Intent(this@MainActivity, HomeActivity::class.java)
+                startActivity(intent)
+            },
+            2000
+        )
     }
 }
