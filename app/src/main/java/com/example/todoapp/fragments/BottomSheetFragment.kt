e69c15b457fabdca373277b7e3461368da264554
ahmedfikry <ahmedfikryelshimi24@gmail.com>
2023-01-28 20:06:58 +0200

    + bottom sheet Screen + create bottom sheet logic

--- app/src/main/java/com/example/todoapp/fragments/BottomSheetFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/BottomSheetFragment.kt
@@ -1,14 +1,21 @@
 package com.example.todoapp.fragments
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.app.DatePickerDialog.OnDateSetListener
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.TextView
 import com.example.todoapp.R
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import java.util.*
 
 class BottomSheet : BottomSheetDialogFragment() {
-
+    lateinit var date: TextView
+    var timeNow: Calendar = Calendar.getInstance()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,5 +25,32 @@ class BottomSheet : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        init(view)
+        initListeners()
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun init(view: View) {
+        date = view.findViewById(R.id.date_text)
+        date.text = "${timeNow.get(Calendar.DAY_OF_MONTH)} / ${timeNow.get(Calendar.MONTH) + 1} / ${
+            timeNow.get(Calendar.YEAR)
+        }"
+    }
+
+    fun initListeners() {
+        date.setOnClickListener {
+            DatePickerDialog(
+                requireContext(),
+                object : OnDateSetListener {
+                    override fun onDateSet(p0: DatePicker?, year: Int, month: Int, day: Int) {
+                        date.text = "$day / ${month + 1} / $year"
+                    }
+                },
+                timeNow.get(Calendar.YEAR),
+                timeNow.get(Calendar.MONTH),
+                timeNow.get(Calendar.DAY_OF_MONTH)
+            ).show()
+        }
     }
 }
